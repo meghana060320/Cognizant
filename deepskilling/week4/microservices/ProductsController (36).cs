fb2c97d76a5dfb6426e025d555dcbad87fa32b78
meghana060320 <231fa04e64@gmail.com>
2026-07-27 22:21:39 +0530

    Add files via upload

--- deepskilling/week4/microservices/ProductsController (36).cs
+++ deepskilling/week4/microservices/ProductsController (36).cs
@@ -3,10 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Runs independently of ProductService, on a different port -
-// each service can be deployed, scaled, and restarted without affecting the other.
-builder.WebHost.UseUrls("http://localhost:5002");
+builder.WebHost.UseUrls("http://localhost:5000"); // the registry itself has a well-known, fixed address
 
 var app = builder.Build();
 
